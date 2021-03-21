757c47ebaa66dcb8db1b2420114ae2a23e1139e7
Niel McLaren <d@nielmclaren.com>
2021-03-20 18:47:11 -0700

    Add DynamoDB.

---
+++ storage/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-2"
+  version = "~>3.0.0"
+}
+
+resource aws_dynamodb_table reflect_table {
+  name         = "ReflectTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+}
