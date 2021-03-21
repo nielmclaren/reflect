6d32b4b1ba1a1e720b3a9eba4a29897f3d94070f
Niel McLaren <d@nielmclaren.com>
2021-03-20 19:37:51 -0700

    Update entry from frontend all the way to DynamoDB.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,36 @@ import "./App.css";
 
 export default function App() {
   const [body, setBody] = useState("");
+  const [entry, setEntry] = useState({});
 
   useEffect(() => {
     console.log("use effect");
     async function effect() {
-      console.log("getting entry");
       const entry = await getEntry('2021-01-01');
-      console.log(entry);
+      console.log("entry", entry);
       setBody(entry.body);
+      setEntry(entry);
     }
     effect();
   }, []);
 
-  function handleSubmit() {
+  async function handleSubmit() {
     console.log("Handle submit.");
+    const prevEntry = Object.assign({}, entry); // Shallow copy only.
+    const nextEntry = {
+      entryId: '2021-01-01',
+      body: body,
+    };
+
+    // Optimistic
+    setEntry(nextEntry);
+
+    if (await postEntry(nextEntry)) {
+      console.log("Success");
+    } else {
+      console.log("Failure");
+      setEntry(prevEntry);
+    }
   }
 
   return (
@@ -26,13 +42,14 @@ export default function App() {
           Body:
           <textarea value={body} onChange={event => setBody(event.target.value)} />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="button" value="Submit" onClick={event => handleSubmit()} />
       </form>
     </div>
   );
 }
 
 async function getEntry(entryId) {
+  console.log("getEntry", entryId);
   const url = "https://reflect-api.nielmclaren.com/api/v1/entries/2021-01-01";
   const options = {
     method: "GET",
@@ -41,11 +58,29 @@ async function getEntry(entryId) {
       "Accept": "application/json",
       "Content-Type": "application/json;charset=UTF-8"
     },
-    //body: JSON.stringify({})
   };
   const response = await fetch(url, options);
   if (response && response.ok) {
     return await response.json();
+  }
+  return null;
+}
+
+async function postEntry(entry) {
+  console.log("postEntry", entry);
+  const url = `https://reflect-api.nielmclaren.com/api/v1/entries/${entry.entryId}`;
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json;charset=UTF-8"
+    },
+    body: JSON.stringify(entry)
+  };
+  const response = await fetch(url, options);
+  if (response && response.ok) {
+    return true;
   }
   return null;
 }
